b784068d084fb5ee5b268fa1f30864e3f954bf98
Jose Luis Neira <jlneira99@gmail.com>
2024-09-06 12:48:03 +0200

    create joint login/signup page and scss

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,38 +1,51 @@
-import styles from "./LogIn.module.scss"
+import React, { useState } from 'react';
+import styles from './LogIn.module.scss'; 
 
 export default function LogIn() {
+  const [isSignUp, setIsSignUp] = useState(true);
 
-    return (
-        <div className={styles.loginContainer}>
-      <h1 className={styles.h1}>Log In</h1>
-      <form className={styles.loginForm}>
-        <div className={styles.formGroup}>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            required
-            placeholder="Enter your email"
-            className={styles.inputField}
-          />
+  const handleSignInClick = () => {
+    setIsSignUp(false);
+  };
+
+  const handleSignUpClick = () => {
+    setIsSignUp(true);
+  };
+
+  return (
+    <div className={styles.LogIn}>
+      <div className={`${styles.container} ${isSignUp ? styles.rightPanelActive : ''}`}>
+        <div className={`${styles.containerForm} ${styles.containerSignup}`}>
+          <form action="#" className={styles.form} id="form1">
+            <h2 className={styles.formTitle}>Sign Up</h2>
+            <input type="text" placeholder="User" className={styles.input} />
+            <input type="email" placeholder="Email" className={styles.input} />
+            <input type="password" placeholder="Password" className={styles.input} />
+            <button className={styles.btn}>Sign Up</button>
+          </form>
         </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            required
-            placeholder="Enter your password"
-            className={styles.inputField}
-          />
+
+        <div className={`${styles.containerForm} ${styles.containerSignin}`}>
+          <form action="#" className={styles.form} id="form2">
+            <h2 className={styles.formTitle}>Sign In</h2>
+            <input type="email" placeholder="Email" className={styles.input} />
+            <input type="password" placeholder="Password" className={styles.input} />
+            <a href="#" className={styles.link}>Forgot your password?</a>
+            <button className={styles.btn}>Sign In</button>
+          </form>
         </div>
-        <button type="submit" className={styles.loginButton}>
-          Log In
-        </button>
-      </form>
+
+        <div className={styles.containerOverlay}>
+          <div className={styles.overlay}>
+            <div className={`${styles.overlayPanel} ${styles.overlayLeft}`}>
+              <button className={styles.btn} onClick={handleSignInClick}>Sign In</button>
+            </div>
+            <div className={`${styles.overlayPanel} ${styles.overlayRight}`}>
+              <button className={styles.btn} onClick={handleSignUpClick}>Sign Up</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    )
-    
+  );
 }
