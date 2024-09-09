394540899cb8b7c891e0d2a866355ec31e4bf085
Mael Micout <micoutmael3@gmail.com>
2024-09-09 16:59:48 +0200

    finalize homepage and details page, add new released list after carousel

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,15 +1,56 @@
-
-
 import CarouselPage from "../../components/Carousel/Carousel";
+import styles from "./Homepage.module.scss";
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { useState, useEffect } from "react";
 
 export default function Homepage() {
 
-    return (
+  const [games, setGames] = useState([]);
+  const navigate = useNavigate(); 
+
+  useEffect(() => {
+    axios.get("http://localhost:5005/api/games/")
+      .then(response => setGames(response.data))
+      .catch(error => console.error("Error fetching featured games:", error));
+  }, []);
+
+  const handleGameClick = (gameId) => {
+    navigate(`/games/${gameId}`);
+  };
+
+  return (
+    <div>
+      <div>
+        <CarouselPage />
+      </div>
+      <div className={styles.releases}>
         <div>
-            <div>
-            <CarouselPage />
-            </div>
-            
+          <h1>New releases</h1>
         </div>
-    )
+        <div className={styles.gameReleased}>
+        <div className={styles.gamesGrid}>
+          {games.slice().reverse().slice(0, 6).map((game) => (
+            <div
+            key={game.id}
+            className={styles.gameCard}
+            onClick={() => handleGameClick(game._id)}
+            style={{
+              backgroundImage: `url(${game.image})`,
+              backgroundSize: 'cover', 
+              backgroundPosition: 'center', 
+              backgroundRepeat: 'no-repeat', 
+            }}
+          >
+          
+            <div className={styles.overlay}>
+            <h1 className={styles.gameTitle}>{game?.name}</h1>
+            </div>
+            </div>
+          ))}
+        </div>
+        </div>
+      </div>
+    </div>
+  );
 }
